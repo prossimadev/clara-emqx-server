cb6374b24b8eafc7464d4d236614b7cd89186c32
root@clara-mqtt-videocall <root@clara-mqtt-videocall>
2026-09-17 15:16:34 +0200

    fix renew

--- lets-encrypt-renew-certificate.sh
+++ lets-encrypt-renew-certificate.sh
@@ -6,3 +6,10 @@ docker run --rm --name certbot -v "/etc/letsencrypt:/etc/letsencrypt" -p 80:80 c
 ## Restart listners
 docker exec emqx-server emqx ctl listeners restart ssl:default
 docker exec emqx-server emqx ctl listeners restart wss:default
+
+## Forza Caddy a rileggere certificati e configurazione
+docker exec caddy-proxy \
+    caddy reload \
+    --force \
+    --config /etc/caddy/Caddyfile \
+    --adapter caddyfile
